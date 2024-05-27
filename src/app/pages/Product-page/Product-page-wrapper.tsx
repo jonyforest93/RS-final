c465e51ce0aabc0aae93694b04862ca2d91ef9a4
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-05-27 10:11:02 +0300

    feat: add getKeyFromUrl function

--- src/app/pages/Product-page/Product-page-wrapper.tsx
+++ src/app/pages/Product-page/Product-page-wrapper.tsx
@@ -9,29 +9,35 @@ import type { IproductData } from 'types/types'
 export const ProductWrapper: React.FC = () => {
   const [productData, setProductData] = useState<IproductData | null>(null)
   const [isLoading, setIsLoading] = useState(true)
-
+  function getKeyFromUrl(): string | null {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get('key')
+  }
+  const productKey: string | null = getKeyFromUrl()
   useEffect(() => {
-    getProductByKey('peonies-with-gypsophila')
-      .then(res => {
-        const dataProduct = res.masterData.current
-        const priceProduct = dataProduct.masterVariant.prices?.[0]
-        if (dataProduct.masterVariant.images && dataProduct.description && priceProduct) {
-          const images = dataProduct.masterVariant.images.map(image => image.url)
-          setProductData({
-            slides: images,
-            name: dataProduct.name['en-US'],
-            description: dataProduct.description['en-US'],
-            price: priceProduct.value.centAmount,
-            discount: priceProduct.discounted?.value.centAmount,
-          })
-        }
-        setIsLoading(false)
-      })
-      .catch(err => {
-        if (err instanceof Error) {
-          console.error(err.message)
-        }
-      })
+    if (productKey) {
+      getProductByKey(productKey) //peonies-with-gypsophila
+        .then(res => {
+          const dataProduct = res.masterData.current
+          const priceProduct = dataProduct.masterVariant.prices?.[0]
+          if (dataProduct.masterVariant.images && dataProduct.description && priceProduct) {
+            const images = dataProduct.masterVariant.images.map(image => image.url)
+            setProductData({
+              slides: images,
+              name: dataProduct.name['en-US'],
+              description: dataProduct.description['en-US'],
+              price: priceProduct.value.centAmount,
+              discount: priceProduct.discounted?.value.centAmount,
+            })
+          }
+          setIsLoading(false)
+        })
+        .catch(err => {
+          if (err instanceof Error) {
+            console.error(err.message)
+          }
+        })
+    }
   }, [])
 
   if (isLoading) {
