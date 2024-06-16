a384085f36d47b7c8642c6179860be091ee5d189
Alex <129378961+Visheyt@users.noreply.github.com>
2024-06-16 23:35:16 +0500

    feat: add promocodes feature (#63)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,45 +13,38 @@ import { CART_KEY, TOKEN_KEY, localStorageService } from 'services/local-storage
 import { CatalogPage } from 'pages/Catalog-page/Catalog-page'
 import { ProductWrapper } from 'pages/ProductPage/ProductPageWrapper'
 import { AboutUsPage } from 'pages/AboutUsPage/AboutUsPage'
-import { createCart, getCartItems } from 'api/cart/getCartItems'
+import { createCart } from 'api/cart/getCartItems'
 import { CartWrapper } from 'pages/Cart-page/CartWrapper'
 import { getActiveCart } from 'api/cart/getActiveCart'
+import { setLoggedUserCartItems } from 'utils/setLoggedUserCartItems'
 
 const App: FC = () => {
   const [isLoggedUser, setIsLoggedUser] = useState(Boolean(localStorageService.getItem(TOKEN_KEY)))
   const [cartItems, setСartItems] = useState<number>(0)
+
   useEffect(() => {
     const token = localStorageService.getItem(TOKEN_KEY)
     if (token) {
       getActiveCart(token)
         .then(res => {
-          localStorageService.setItem(CART_KEY, res.body?.id as string)
+          const cartKey = res.body?.id as string
+          localStorageService.setItem(CART_KEY, cartKey)
+          setLoggedUserCartItems(cartKey, setСartItems)
         })
         .catch(() => {
           createCart()
             .then(res => {
-              localStorageService.setItem(CART_KEY, res.body?.id as string)
+              const cartKey = res.body?.id as string
+              localStorageService.setItem(CART_KEY, cartKey)
+              setLoggedUserCartItems(cartKey, setСartItems)
             })
             .catch(err => {
               console.error(err)
             })
         })
     }
-  }, [])
+  }, [isLoggedUser])
 
-  useEffect(() => {
-    const cartKey = localStorageService.getItem(CART_KEY)
-    if (!cartKey) {
-      return
-    }
-    getCartItems(cartKey)
-      .then(res => {
-        setСartItems(res.lineItems.length)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-  }, [])
   return (
     <cartItemsContext.Provider value={{ cartItems, setСartItems }}>
       <loginContext.Provider value={{ isLoggedUser, setIsLoggedUser }}>
